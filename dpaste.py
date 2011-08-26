b5aec8d0b38eafcf2a02257c68ceec1452c84695
Brian Riley <brian@btriley.com>
2011-08-25 23:38:50 -0700

    Adding README and dpaste plugin

---
+++ dpaste.py
@@ -0,0 +1,34 @@
+import urllib, httplib
+
+import sublime_plugin, sublime
+
+
+class DpasteCommand(sublime_plugin.TextCommand):
+    """
+    Pastes the selected test to dpaste.com and copies the URL to the clipboard
+    """
+    SYNTAXES = {
+        'py': 'Python',
+        'sql': 'Sql',
+        'js': 'JScript',
+        'css': 'Css',
+        'xml': 'Xml',
+        'diff': 'Diff',
+        'rb': 'Ruby',
+        'rhtml': 'Rhtml',
+        'hs': 'Haskell',
+        'sh': 'Bash'
+    }
+
+    def run(self, edit):
+        params = urllib.urlencode({
+            'content': '\n'.join([self.view.substr(region) for region in self.view.sel()]),
+            'language': DpasteCommand.SYNTAXES.get(self.view.file_name().split('.')[-1], '')
+        })
+        
+        connection= httplib.HTTPConnection('dpaste.com')
+        connection.request('POST', '/api/v1/', params)
+        response = connection.getresponse()
+
+        sublime.set_clipboard(response.getheader('Location', ''))
+        sublime.status_message("Dpaste URL has been copied to clipboard")
